b9823e8697022fa4a77498090bbbef4f6b07e517
student <nikitamendukov@gmail.com>
2023-06-01 20:06:37 +0300

    ex 9

--- main.py
+++ main.py
@@ -2,10 +2,9 @@
 l2 = []
 def f(l):
     l1 = l.split(' ')
-    print(l1)
     for i in l1:
         if 'Python' in i:
             l2.append(i)
-print(f'Список з слів, що мають у собі "Python": {l2}')
 l = input('Введіть слова через пробіл:')
 f(l)
+print(f'Список з слів, що мають у собі "Python": {l2}')
