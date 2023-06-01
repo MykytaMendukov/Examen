0c2a5fde292e008ba89c66fb94a8c05af5cfe4f0
student <nikitamendukov@gmail.com>
2023-06-01 19:05:45 +0300

    ex 1 -2

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+#Частина 1: Основи Python
+
+#1
+print('Hello World!')
+
+#2
+a = int(input('Введіть перше число: '))
+b = int(input('Введіть друге число: '))
+s = 0
+for i in range(a, b + 1):
+    s+= i
+print(s)
 
