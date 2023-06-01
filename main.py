551fc243f6785bbb9e7139756d2acab9f2da3d5f
student <nikitamendukov@gmail.com>
2023-06-01 19:24:52 +0300

    ex 6

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-a = input('Введіть слово: ')
-for letter in a:
-    print(letter)
+#6
+import random
+l = []
+for i in range(6):
+    i = random.randint(1, 10)
+    l.append((i))
+print(f'Початковий список: {l}')
+l1 = []
+for n in l:
+    if n % 2 == 0:
+        l1.append(n)
+print(f'писок з парними числами: {l1}')
